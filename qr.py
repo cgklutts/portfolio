e1617e22eaec37bb17e413a1d89ae16c330fa312
chris <cgklutts@gmail.com>
2025-08-18 16:09:19 -0500

    19

---
+++ qr.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# URL to encode in the QR code
+url = "https://cgklutts.github.io/portfolio/"
+
+# Create QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code image
+img.save("qrcode.png")
